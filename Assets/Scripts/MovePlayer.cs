ac82642743a2c1b7569ba3b8a13ce872bbb8a659
Samuel Wilson <swi125@uclive.ac.nz>
2021-09-16 19:51:57 +1200

    Adding player swapping positions

---
+++ Assets/Scripts/MovePlayer.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovePlayer : MonoBehaviour
+{
+    public float direction;
+    public Rigidbody2D rb;
+    public float playerSpeed;
+    public GameObject opposite;
+    public GameObject screen;
+    private Vector3 opp_pos;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        direction = Input.GetAxis("Horizontal");
+        rb.velocity = new Vector2(direction * playerSpeed, 0);
+        if (Input.GetKeyDown("space"))
+        {
+            opposite.SetActive(false);
+            opp_pos = opposite.transform.position;
+            opposite.transform.position = this.transform.position;
+            this.transform.position = opp_pos;
+            opposite.SetActive(true);
+        }
+        
+    }
+}

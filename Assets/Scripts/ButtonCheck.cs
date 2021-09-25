918af1006e22d418bbc8753b82e87205e0403917
Bree Merriman <tme67@uclive.ac.nz>
2021-09-26 08:48:54 +1300

    Added button prefab

---
+++ Assets/Scripts/ButtonCheck.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonCheck : MonoBehaviour
+{
+    private Animator animator;
+    private void Start() {
+        animator = GetComponent<Animator>();
+    }
+    private void OnTriggerEnter2D(Collider2D collision) {
+        
+        //Checks whether the 'cat' that pushed the button is the live cat
+		if (collision.gameObject.tag == "Live_Cat")
+		{
+			animator.SetBool("pushed", true);
+
+            //Button push function for interactibles here: opening door, activating mechanism, etc.
+                //Can recode to handle button pushes in the interactible script (will make this script constant), but this works for now
+        }
+    }
+    private void OnTriggerExit2D(Collider2D collision) {
+        animator.SetBool("pushed", false);
+    }
+}
